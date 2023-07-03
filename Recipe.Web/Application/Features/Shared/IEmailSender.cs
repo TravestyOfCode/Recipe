09d2e0dceebaf15136d749f3fcec534f58d05b3e
travis@travestyofcode.com <travis@travestyofcode.com>
2023-07-03 14:27:12 -0400

    Adds placeholder EmailSender which does not send emails.

---
+++ Recipe.Web/Application/Features/Shared/IEmailSender.cs
@@ -0,0 +1,14 @@
+﻿namespace Recipe.Web.Application.Features.Shared;
+
+public interface IEmailSender
+{
+    Task SendEmailAsync(string email, string subject, string htmlMessage);
+}
+
+public class NoOpEmailSender : IEmailSender
+{
+    public Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        return Task.CompletedTask;
+    }
+}
