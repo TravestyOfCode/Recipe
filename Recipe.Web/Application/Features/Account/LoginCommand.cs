21b3ba54b9be7f3f249cda66742d964a57858ba4
travis@travestyofcode.com <travis@travestyofcode.com>
2023-07-03 15:52:48 -0400

    Adds Login controller, commands and views.

---
+++ Recipe.Web/Application/Features/Account/LoginCommand.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Recipe.Web.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace Recipe.Web.Application.Features.Account;
+
+public class LoginCommand : IRequest<Result>
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    public bool RememberMe { get; set; }
+}
+
+public class LoginCommandHandler : IRequestHandler<LoginCommand, Result>
+{
+    private readonly SignInManager<AppUser> signInManager;
+
+    private readonly ILogger<LoginCommandHandler> logger;
+
+    public LoginCommandHandler(SignInManager<AppUser> signInManager, ILogger<LoginCommandHandler> logger)
+    {
+        this.signInManager = signInManager;
+        this.logger = logger;
+    }
+
+    public async Task<Result> Handle(LoginCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var result = await signInManager.PasswordSignInAsync(request.Email, request.Password, request.RememberMe, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                logger.LogInformation("User logged in with {email}", request.Email);
+
+                return Result.Ok();
+            }
+
+            return Result.BadRequest(new Error(string.Empty, "Invalid UserName or Password."));
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Unexpected error handling {request}.", request);
+
+            return Result.ServerError();
+        }
+    }
+}
---
+++ Recipe.Web/Areas/Account/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Recipe.Web.Application.Features.Account;
+
+namespace Recipe.Web.Areas.Account.Controllers;
+
+[AllowAnonymous]
+[Area("Account")]
+public class LoginController : Controller
+{
+    private readonly IMediator mediator;
+
+    public LoginController(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Index(LoginCommand request, string returnUrl, CancellationToken cancellationToken)
+    {
+        returnUrl ??= Url.Content("~/");
+
+        var result = await mediator.Send(request, cancellationToken);
+
+        if (result.WasSuccessful)
+        {
+            return LocalRedirect(returnUrl);
+        }
+
+        if (result.IsBadRequest)
+        {
+            ModelState.AddErrors(result);
+
+            return View();
+        }
+
+        return StatusCode(result.StatusCode);
+    }
+}
+
