07c51a0e5785670b7a17321f95016f4f1b8b2f5d
travis@travestyofcode.com <travis@travestyofcode.com>
2023-07-04 06:19:32 -0400

    Adds data Category and migration.

---
+++ Recipe.Web/Data/Category.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Recipe.Web.Data;
+
+public class Category
+{
+    public int Id { get; set; }
+
+    public string UserId { get; set; }
+
+    public AppUser User { get; set; }
+
+    public string Name { get; set; }
+}
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.ToTable(nameof(Category));
+
+        builder.Property(p => p.Name)
+            .IsRequired(true)
+            .HasMaxLength(32);
+
+        builder.HasIndex(p => new { p.Name, p.UserId });
+    }
+}
---
+++ Recipe.Web/Data/Migrations/20230704095804_Add-Cateogry.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Recipe.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCateogry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Category_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Category_Name_UserId",
+                table: "Category",
+                columns: new[] { "Name", "UserId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Category_UserId",
+                table: "Category",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Category");
+        }
+    }
+}
