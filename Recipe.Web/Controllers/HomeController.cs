3f13f5eeb727386b214377039219036dd7585ec1
travis@travestyofcode.com <travis@travestyofcode.com>
2023-07-07 14:38:37 -0400

    Renames Models to ViewModels and moves existing view models. Updates usings for ViewModels.

--- Recipe.Web/Controllers/HomeController.cs
+++ Recipe.Web/Controllers/HomeController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Recipe.Web.Models;
+using Recipe.Web.ViewModels;
 using System.Diagnostics;
 
 namespace Recipe.Web.Controllers;
---
+++ Recipe.Web/ViewModels/Ingredient/IngredientAddViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Recipe.Web.ViewModels.Ingredient;
+
+public class IngredientAddViewModel
+{
+    public Dictionary<int, string> UnitOfMeasrues { get; set; }
+
+    public string NamePrefix { get; set; }
+}
